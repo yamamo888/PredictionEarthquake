c7b6dd8db25e5c555d4a74276c7e99658eb05b55
Hirotaka Hachiya <hacchan@gmail.com>
2018-05-15 16:36:21 +0900

    increment by 0.0025

--- changeB.bat
+++ changeB.bat
@@ -1,11 +1,13 @@
 @setlocal enabledelayedexpansion
 
 set /A b0=0
-set /A b0_base=14460
+@set /A b0_base=14460 %0.014460
+set /A b0_base=11000 %0.012000
 
-for /L %%i in (1, 1, 10) do (
-	set /A b0 += b0_base + %%i * 100
+for /L %%i in (0, 1, 240) do (
+	set /A b0 = b0_base + %%i * 25
 	echo !b0!
-	..\x64\Release\CycleBranch2.exe !b0! 13990 12855 13239 13195 12945 13844 16970 > log_%%i.txt
+	..\x64\Release\CycleBranch2.exe !b0! 13990 12855 13239 13195 12945 13844 16970 > log_25_%%i.txt
 )
 @pause
+
