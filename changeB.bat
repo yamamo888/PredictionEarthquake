752f2cc6cc8ca4d2bc6f3f3fcb9b3be697dcab31
yamamo888 <s206292@center.wakayama-u.ac.jp>
2018-06-29 17:49:07 +0900

    KDEをつけくわえたもの

--- changeB.bat
+++ changeB.bat
@@ -4,10 +4,10 @@ set /A b0=0
 @set /A b0_base=14460 %0.014460
 set /A b0_base=11000 %0.012000
 
-for /L %%i in (0, 1, 240) do (
-	set /A b0 = b0_base + %%i * 25
+for /L %%i in (0, 1, 800) do (
+	set /A b0 = b0_base + %%i * 5
 	echo !b0!
-	..\x64\Release\CycleBranch2.exe !b0! 13990 12855 13239 13195 12945 13844 16970 > log_25_%%i.txt
+	..\x64\Release\CycleBranch2.exe !b0! 13990 12855 13239 13195 12945 13844 16970 > log_0_%%i.txt
 )
 @pause
 
